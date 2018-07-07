a0147c8a1b0ee35741fb3d23c66f1e158360ce57
Evgeniy <evgeniy.sapov@gmail.com>
2018-07-08 04:07:05 +0500

    Second one

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+    
 import styled from 'styled-components';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+const  HeaderWrapper = styled.header`
+width: 100%;
+height: 49px;
+background-color: #242424;
+opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+height: 89px;
+padding-top:21px;
+`
+const MainWrapper = styled.main`
+padding-top: 130px;
+padding-bottom: 140px;
+background: url(${url}) no-repeat;
+background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <p> Ma booooy </p>
-        <Hello name="evgeniy" surname="datguy" />
-          <h1> HELLO WORLD !</h1>
-      </div>
+          <div className="App">
+           <HeaderWrapper>
+                <Grid>   
+                     <Header/>
+                  </Grid>    
+             </HeaderWrapper>
+             <MenuWrapper>
+                <Grid>
+                     <Menu/>
+                </Grid>
+            </MenuWrapper>       
+            <MainWrapper>
+                <Grid>                           
+                        <Main/>
+                </Grid>
+            </MainWrapper>  
+          </div>
     );
   }
 }
-
-function Hello(props) {
-   return ( 
-     <div> 
-     <h1> Мое имя {props.name} фамилия  {props.surname} </h1>
-      <a href= "{props.name}">ссылка на мой профиль </a>
-     </div>
-    )
-   }
 
 
 export default App;
