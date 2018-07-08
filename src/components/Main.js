3ad7a8d7dcf1c6fc357b3ad07037c355378929cf
Evgeniy <evgeniy.sapov@gmail.com>
2018-07-08 14:43:49 +0500

    Develop

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-	
+import Slider from './Slider'	
 const Repair = styled.div`
 color: #ffffff;
 font-size: 40px;
@@ -51,7 +51,7 @@ class Main extends React.Component {
 						 	<CallBtn> Отправить заявку! </CallBtn>
 						 </Col>
 						 <Col lg={6} lgOffset={1}>
-
+									<Slider />
 						 </Col>
 						</Row>
 			)
